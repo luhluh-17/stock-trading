eb1f81d2462d38ba7b262a5dbb6c5acddf12ad4a
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-10-08 11:49:32 +0800

    migration for confirmable functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -76,8 +76,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => <gmail_username>,
-    :password             => <gmail_password>,
+    :user_name            => "davelavilla@gmail.com",
+    :password             => "ykureoysrnrzpcpr",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
