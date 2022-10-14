922f417a1d190a979d95b5782c9f1ba0ca01a4a6
luhluh-17 <castro.ralph17@gmail.com>
2022-10-14 21:02:12 +0800

    run db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,64 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_014_120_651) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_120651) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'symbol'
-    t.float 'amount'
-    t.integer 'percentage'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_products_on_user_id'
+  create_table "products", force: :cascade do |t|
+    t.string "symbol"
+    t.float "amount"
+    t.integer "percentage"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table 'stocks', force: :cascade do |t|
-    t.string 'symbol'
-    t.float 'amount'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_stocks_on_user_id'
+  create_table "stocks", force: :cascade do |t|
+    t.string "symbol"
+    t.float "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
   end
 
-  create_table 'transactions', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'product_id', null: false
-    t.integer 'price'
-    t.integer 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['product_id'], name: 'index_transactions_on_product_id'
-    t.index ['user_id'], name: 'index_transactions_on_user_id'
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "price"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_transactions_on_product_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'role', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.float 'balance', default: 0.0
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'status', default: 'pending', null: false
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "balance", default: 0.0
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "status", default: "pending", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'products', 'users'
-  add_foreign_key 'stocks', 'users'
-  add_foreign_key 'transactions', 'products'
-  add_foreign_key 'transactions', 'users'
+  add_foreign_key "products", "users"
+  add_foreign_key "stocks", "users"
+  add_foreign_key "transactions", "products"
+  add_foreign_key "transactions", "users"
 end
